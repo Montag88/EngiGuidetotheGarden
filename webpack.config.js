948f0a716bf18ad5ebd58cbaf10aeeca42cbb9fd
Patrick Wong <pwong288@gmail.com>
2019-11-21 23:47:28 -0800

    Iniialize repo folder and configure webpack with babel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client', 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  mode: 'development',
+};
